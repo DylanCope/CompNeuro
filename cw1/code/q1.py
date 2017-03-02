2d9bb7df4fd3878d26a4996791f09d32466d46da
Dylan Cope <dylanr.cope@gmail.com>
2017-03-02 03:01:53 +0000

    changed q1 to have oop approach

--- cw1/code/q1.py
+++ cw1/code/q1.py
@@ -2,30 +2,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 from operator import iadd
 
+class Neuron:
+
+    def __init__( self ):
+
+        # constant parameters
+        self.tau_m = 10e-3 # s (membrane time constant)
+        self.E_L = self.V_r = -70e-3 # V (leak potential and rest voltage)
+        self.V_t = -40e-3 # V (spiking threshold)
+        self.R_m = 10e6 # Ohms (membrane resistance)
+        self.I_e = 3.1e-9 # A (injected current ('e' for electrode))
+
+        # neuron variables
+        self.V = self.V_r # V (internal voltage within the neuron)
+
+    def update( self, dt ):
+        ''' updates and returns the neuron's internal voltage value, the updated value represents the voltage at the next time step
+        '''
+
+        dvdt = (self.E_L - self.V + self.R_m * self.I_e) / self.tau_m
+        if self.V < self.V_t:
+            self.V = self.V + dvdt * dt
+        else:
+            self.V = self.V_r
+
+        return self.V
+
 if __name__ == '__main__':
 
-    # neuron parameters
-    tau_m = 10e-3 # s (membrane time constant)
-    E_L = V_r = -70e-3 # V (leak potential and rest voltage)
-    V_t = -40e-3 # V (spiking threshold)
-    R_m = 10e6 # Ohms (membrane resistance)
-    I_e = 3.1e-9 # A (injected current ('e' for electrode))
-
     # Using the integrate and fire model this code plots voltage in the cell over time, no firing behaivour or refractory rest period is simulated for simplicity. Once the membrane potential exceeds threshold it's value is set to V_r
+
+    neuron = Neuron()
 
     dt = 1e-3 # s (time step for Euler method)
     T = 1 # s (finish time simulation)
 
-    v = V_r
-
-    def V( t ):
-        global v
-        dvdt = (E_L - v + R_m*I_e) / tau_m
-        v = v + dvdt * dt if v < V_t else V_r
-        return v
-
     ts = [ i * dt for i in range( 1 + int( T / dt ) ) ]
-    vs = [ V(t) for t in ts ]
+    vs = [ neuron.update(dt) for t in ts ]
 
     fig, ax = plt.subplots()
     ax.set_ylabel( 'Voltage (V)' )
