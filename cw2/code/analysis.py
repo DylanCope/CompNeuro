2870ea9d29c958b14b9495ee73f072e8c0410a9e
Dylan Cope <dylanr.cope@gmail.com>
2017-04-16 12:28:58 +0100

    cross correlations for neuron firing rates

--- cw2/code/analysis.py
+++ cw2/code/analysis.py
@@ -1,7 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from itertools import product
 
 def load( name ):
+    '''
+    '''
     with open( '../data/%s.csv' % name ) as f:
         data = map( lambda x: float(x.strip()), f.readlines() )
     return np.array([ *data ])
@@ -13,12 +16,37 @@ def fire_rate( fire_times ):
     fire_times = np.int64( fire_times )
     return np.bincount( fire_times )
 
+def plot_correlations():
+    '''
+    '''
+    fig, axs = plt.subplots( 4, 4 )
+    frs = [ fire_rate(load( 'neuron%d' % i ))
+            for i in range( 1, 5 ) ]
+    max_steps = max( fr.size for fr in frs )
+    frs = [ np.concatenate(( fr, np.zeros((max_steps - fr.size)) ))
+            for fr in frs ]
+    kernel = np.ones(100) / 100
+    for i, j in product( range(1, 5), range(1, 5) ):
+        x = frs[ i - 1 ]
+        y = frs[ j - 1 ]
+        cor = np.correlate( x, y, mode = 'full' )
+        cor_smooth = np.convolve( cor, kernel, mode = 'same' )
+        cor = cor[ cor.size // 2 : ][ : 1200 ]
+        cor_smooth  = cor_smooth[ cor_smooth.size // 2 : ][ : 1200 ]
+        ax = axs[ i - 1, j - 1 ]
+        ax.plot( cor, c='b', alpha=0.5 )
+        ax.plot( cor_smooth, c='r' )
+        ax.set_xlabel( 'Lag ($10^{-4} s$)' );
+        if i == j:
+            ax.set_title( 'Auto-correlation for Neuron %d' % i )
+            ax.set_ylabel( 'Fire Rate Auto-correlation ($10^{-4} s^{-2}$)' )
+        else:
+            ax.set_title( 'Cross-correlation between Neurons %d and %d' % (i, j) )
+            ax.set_ylabel( 'Fire Rate Cross-correlation ($10^{-4} s^{-2}$)' )
+    plt.show()
+
 def fired_indices( time_stamps, fire_times ):
     '''
-        Arguments:
-
-        Returns:
-
     '''
     # indices in x, y and t closest to when neuron fired
     find = np.vectorize( lambda v : np.argmin(np.abs( time_stamps - v )) )
@@ -27,19 +55,11 @@ def fired_indices( time_stamps, fire_times ):
 
 def firing_positions( fire_times ):
     '''
-        Arguments:
-
-        Returns:
-
     '''
-
     time_stamps = load( 'time' )
     fired_idxs = fired_indices( time_stamps, fire_times )
-
-    x = load( 'x' )
-    y = load( 'y' )
-
-    X = np.vstack(( x[ fired_idxs ], y[ fired_idxs ] )).T
+    X = np.vstack(( load( 'x' )[ fired_idxs ],
+                    load( 'y' )[ fired_idxs ] )).T
     return X
 
 def spatial_fire_rate_matrix( fire_times ):
@@ -70,9 +90,9 @@ def show_path( steps = -1 ):
     '''
     '''
     t = load( 'time' )[ : steps ]
-    y = load( 'y' )[ : steps ]
-    x = load( 'x' )[ : steps ]
-    X = np.vstack(( x, y, (t - t[0]) / 1e4 )).T
+    X = np.vstack(( load( 'x' )[ : steps ],
+                    load( 'y' )[ : steps ],
+                    ( t - t[0] ) / 1e4 )).T
 
     grid_shape = ( 300, 300 )
     grid = np.zeros( grid_shape )
@@ -88,3 +108,23 @@ def show_path( steps = -1 ):
     plt.ylabel( 'Y' )
     plt.title( 'Time at position' )
     plt.show()
+
+def velocity():
+    '''
+    '''
+    X = np.vstack(( load( 'x' ),
+                    load( 'y' ) )).T
+    t = load( 'time' )
+
+    dx = X[ 1: ] - X[ :-1 ]
+    dt = t[ 1: ] - t[ :-1 ]
+    v = dx / np.vstack(( dt, dt )).T
+    return v
+
+def speed():
+    '''
+    '''
+    v = velocity()
+    v = np.sqrt( v[:, 0]**2 + v[:, 1]**2 )
+    v = np.concatenate((v, np.array([0])))
+    return v
